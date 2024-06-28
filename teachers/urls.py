3dab00b3aa4f471e2f6c0455beb5fdbc8e982c95
dnetochaves <dnetochaves@gmail.com>
2024-06-28 07:02:59 -0300

    cadastro de professores

--- teachers/urls.py
+++ teachers/urls.py
@@ -5,7 +5,7 @@ from .views import TeacherList
 app_name = "teachers"
 urlpatterns = [
     path("professores", TeacherList.as_view(), name="list"),
-    # path("professores/<int:pk>", TeacherDetail.as_view(), name="detail"),
+    #path("professores/<int:pk>", TeacherDetail.as_view(), name="detail"),
     # path("me", MeView.as_view(), name="me"),
     # path("professores/foto", TeacherProfileImageView.as_view(), name="profile-image"),
 ]
